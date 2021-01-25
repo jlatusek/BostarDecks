86938492cb2df7940be64b2d5f6c22735c2070ac
Jakub Latusek <jk.latusek@gmail.com>
2021-01-25 02:17:47 +0100

    [LOG] Create logger config
    Signed-off-by: Jakub Latusek <jk.latusek@gmail.com>

---
+++ src/inc/logger.h
@@ -0,0 +1,36 @@
+//
+// Created by jakub on 1/10/21.
+//
+
+#pragma once
+
+#include <boost/log/core.hpp>
+#include <boost/log/expressions.hpp>
+#include <boost/log/sinks/text_file_backend.hpp>
+#include <boost/log/sources/record_ostream.hpp>
+#include <boost/log/sources/severity_logger.hpp>
+#include <boost/log/trivial.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/utility/setup/file.hpp>
+
+namespace logging = boost::log;
+namespace sinks = boost::log::sinks;
+namespace src = boost::log::sources;
+namespace expr = boost::log::expressions;
+namespace attrs = boost::log::attributes;
+namespace keywords = boost::log::keywords;
+
+namespace logger {
+
+void init() {
+  logging::add_file_log(keywords::file_name = "sample_%N.log",
+                        keywords::rotation_size = 10 * 1024 * 1024,
+                        keywords::time_based_rotation =
+                            sinks::file::rotation_at_time_point(0, 0, 0),
+                        keywords::format = "[%TimeStamp%]: %Message%" 4);
+
+  logging::core::get()->set_filter(logging::trivial::severity >=
+                                   logging::trivial::info);
+}
+}  // namespace logger
+
