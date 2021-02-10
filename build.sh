aae9e7878fa65110515b84d08368b8119ae80168
Jakub <jk.latusek@gmail.com>
2021-02-10 19:32:37 +0100

    [BUILD] Use clang to build project
    Signed-off-by: Jakub Latusek <jk.latusek@gmail.com>

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ rm -rf build
 mkdir build
 pushd build
 
-conan install .. --build=missing -s build_type=Debug
-cmake .. -DCMAKE_BUILD_TYPE=Debug
+conan install .. --build=missing -s build_type=Debug --profile clang
+cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++
 cmake --build . -j8
