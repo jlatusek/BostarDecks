d6ca38f19b871ed2286a53127a405158214e819f
Jakub Latusek <jk.latusek@gmail.com>
2021-03-07 15:33:15 +0100

    CU-f94pxc TypeHandler change all implemented function to static type

--- src/db/models/deck.h
+++ src/db/models/deck.h
@@ -3,6 +3,7 @@
 
 #include "Poco/Data/AbstractBinder.h"
 #include "Poco/Data/AbstractExtractor.h"
+#include "Poco/Data/AbstractPreparator.h"
 #include "Poco/Data/TypeHandler.h"
 
 namespace model {
@@ -23,7 +24,8 @@ public:
 };
 }
 
-namespace Poco::Data {
+namespace Poco {
+namespace Data {
 
 template<>
 class TypeHandler<struct model::Deck>
@@ -31,67 +33,48 @@ class TypeHandler<struct model::Deck>
 public:
   static std::size_t size() { return 5; }
 
-  void bind(std::size_t pos,
+  static void bind(std::size_t pos,
             const model::Deck& obj,
             Poco::Data::AbstractBinder::Ptr pBinder,
             Poco::Data::AbstractBinder::Direction dir)
   {
 
-    // the table is defined as Person (LastName VARCHAR(30), FirstName VARCHAR,
-    // Age INTEGER(3)) Note that we advance pos by the number of columns the
-    // datatype uses! For string/int this is one.
     poco_assert_dbg(!pBinder.isNull());
-    TypeHandler<int>::bind(pos++, obj.getId(), pBinder, dir);
-    TypeHandler<std::string>::bind(pos++, obj.getName(), pBinder, dir);
-    TypeHandler<std::string>::bind(pos++, obj.getDescription(), pBinder, dir);
-    TypeHandler<bool>::bind(pos++, obj.isPublic(), pBinder, dir);
-    TypeHandler<std::string>::bind(pos++, obj.getTag(), pBinder, dir);
+    TypeHandler<int>::bind(pos++, obj.id, pBinder, dir);
+    TypeHandler<std::string>::bind(pos++, obj.name, pBinder, dir);
+    TypeHandler<std::string>::bind(pos++, obj.description, pBinder, dir);
+    TypeHandler<bool>::bind(pos++, obj.is_public, pBinder, dir);
+    TypeHandler<std::string>::bind(pos++, obj.tag, pBinder, dir);
   };
 
-  void prepare(std::size_t pos,
+  static void prepare(std::size_t pos,
                const model::Deck& obj,
                Poco::Data::AbstractPreparator::Ptr pPreparator)
   {
 
-    // the table is defined as Person (LastName VARCHAR(30), FirstName VARCHAR,
-    // Age INTEGER(3))
     poco_assert_dbg(!pPreparator.isNull());
-    TypeHandler<int>::prepare(pos++, obj.getId(), pPreparator);
-    TypeHandler<std::string>::prepare(pos++, obj.getName(), pPreparator);
-    TypeHandler<std::string>::prepare(pos++, obj.getDescription(), pPreparator);
-    TypeHandler<bool>::prepare(pos++, obj.isPublic(), pPreparator);
-    TypeHandler<std::string>::prepare(pos++, obj.getTag(), pPreparator);
+    TypeHandler<int>::prepare(pos++, obj.id, pPreparator);
+    TypeHandler<std::string>::prepare(pos++, obj.name, pPreparator);
+    TypeHandler<std::string>::prepare(pos++, obj.description, pPreparator);
+    TypeHandler<bool>::prepare(pos++, obj.is_public, pPreparator);
+    TypeHandler<std::string>::prepare(pos++, obj.tag, pPreparator);
   };
 
-  void extract(std::size_t pos,
+  static void extract(std::size_t pos,
                model::Deck& obj,
                const model::Deck& defVal,
                Poco::Data::AbstractExtractor::Ptr pExt)
   {
 
-    // defVal is the default person we should use if we encunter NULL entries,
-    // so we take the individual fields as defaults. You can do more complex
-    // checking, ie return defVal if only one single entry of the fields is null
-    // etc...
     poco_assert_dbg(!pExt.isNull());
-    int id;
-    std::string name;
-    std::string description;
-    bool public_;
-    std::string tag;
-    // the table is defined as Person (LastName VARCHAR(30), FirstName VARCHAR,
-    // Age INTEGER(3))
-    TypeHandler<int>::extract(pos++, id, defVal.getId(), pExt);
-    TypeHandler<std::string>::extract(pos++, name, defVal.getName(), pExt);
+    TypeHandler<int>::extract(pos++, obj.id, defVal.id, pExt);
+    TypeHandler<std::string>::extract(pos++, obj.name, defVal.name, pExt);
     TypeHandler<std::string>::extract(
-      pos++, description, defVal.getDescription(), pExt);
-    TypeHandler<bool>::extract(pos++, public_, defVal.isPublic(), pExt);
-    TypeHandler<std::string>::extract(pos++, tag, defVal.getTag(), pExt);
-    obj.setId(id);
-    obj.setName(name);
-    obj.setDescription(description);
-    obj.setPublic(public_);
-    obj.setTag(tag);
+      pos++, obj.description, defVal.description, pExt);
+    TypeHandler<bool>::extract(pos++, obj.is_public, defVal.is_public, pExt);
+    TypeHandler<std::string>::extract(pos++, obj.tag, defVal.tag, pExt);
   };
+
 };
-} // namespace Poco
+}
+}
